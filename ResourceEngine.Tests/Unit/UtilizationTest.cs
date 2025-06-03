401bea12140c6ccf3cbe48feecd7dbd095acb660
Fred George <fredgeorge@acm.org>
2025-06-03 08:51:32 +0530

    Fred: Starting Fold logic

--- ResourceEngine.Tests/Unit/UtilizationTest.cs
+++ ResourceEngine.Tests/Unit/UtilizationTest.cs
@@ -14,13 +14,22 @@ public class UtilizationTest {
         Assert.Equal(3.0/8, u.Average(), 1e-5);
     }
 
+    [Fact]
     public void Addition() {
-        
         var u1 = new Utilization(1.May(), 2.Days(), [0,1,1,0,0,0,1,0]);
         var u2 = new Utilization(1.May(), 2.Days(), [0,0,1,1,0,0,1,0]);
         var u3 = new Utilization(1.May(), 2.Days(), [0,0,1,1,0,1,1,0]);
         Utilization uSum = u1 + u2 + u3;
         Assert.Equal(3, uSum.Max());
         Assert.Equal(10.0/8, uSum.Average(), 1e-5);
+    }
+
+    [Fact]
+    public void Fold() {
+        
+        var u = new Utilization(1.May(), 2.Days(), [0,1,1,0,0,0,1,0]);
+        u.Fold(24.Hours());
+        Assert.Equal(2, u.Max());
+        Assert.Equal(3.0/8, u.Average(), 1e-5);
     }
 }
