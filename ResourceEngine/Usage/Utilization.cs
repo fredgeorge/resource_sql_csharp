604c47f99f2435e9734bb7133d4bc016a057d699
Fred George <fredgeorge@acm.org>
2025-06-03 08:19:27 +0530

    Fred: Introduced State Pattern for Utilization

--- ResourceEngine/Usage/Utilization.cs
+++ ResourceEngine/Usage/Utilization.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+
 namespace ResourceEngine.Usage;
 
 // Understands the occupancy pattern for a Resource
@@ -6,6 +8,7 @@ public class Utilization {
     private readonly TimeSpan _duration;
     private readonly OccupationPattern _pattern;
     private readonly TimeSpan _interval;
+    private State _state = Initial.Instance;
 
     public Utilization(DateTime start, TimeSpan duration, List<int> occupancyCounts) {
         _start = start;
@@ -14,9 +17,21 @@ public class Utilization {
         _pattern = new OccupationPattern(occupancyCounts);
     }
 
-    public int Max() => _pattern.Max();
+    public int Max() => _state.Max(this);
 
-    public double Average() => _pattern.Average();
+    public double Average() => _state.Average(this);
+
+    private interface State {
+        int Max(Utilization u);
+        double Average(Utilization u);
+    }
+
+    private class Initial : State {
+        internal static readonly Initial Instance = new Initial();
+        private Initial() {}
+        public int Max(Utilization u) => u._pattern.Max();
+        public double Average(Utilization u) => u._pattern.Average();
+    }
     
     private class OccupationPattern(List<int> occupancyCounts) {
         internal int Max() => occupancyCounts.Max();
