f355581c7a9763a6fe980a4b3b23603893d39456
Fred George <fredgeorge@acm.org>
2025-06-03 08:48:07 +0530

    Fred: Utilization with addition

--- ResourceEngine.Tests/Util/DateTimeExtensions.cs
+++ ResourceEngine.Tests/Util/DateTimeExtensions.cs
@@ -10,8 +10,9 @@ namespace ResourceEngine.Tests.Util;
 
 // Understands Convenience constructs for time, duration, and dates
 internal static class DateTimeExtensions {
-    internal static DateTime May(this int dayOfMonth) => new DateTime(2025, 5, dayOfMonth);
+    internal static DateTime May(this int dayOfMonth) => new(2025, 5, dayOfMonth);
     
-    internal static TimeSpan Days(this int dayCount) => new TimeSpan(dayCount, 0, 0, 0);
+    internal static TimeSpan Days(this int dayCount) => new(dayCount, 0, 0, 0);
+    internal static TimeSpan Hours(this int hourCount) => new(hourCount, 0, 0);
     
 }
--- ResourceEngine/Usage/Utilization.cs
+++ ResourceEngine/Usage/Utilization.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel;
-
 namespace ResourceEngine.Usage;
 
 // Understands the occupancy pattern for a Resource
@@ -10,16 +8,24 @@ public class Utilization {
     private readonly TimeSpan _interval;
     private State _state = Initial.Instance;
 
-    public Utilization(DateTime start, TimeSpan duration, List<int> occupancyCounts) {
+    public Utilization(DateTime start, TimeSpan duration, List<int> occupancyCounts)
+        : this(start, duration, new OccupationPattern(occupancyCounts)) { }
+
+    private Utilization(DateTime start, TimeSpan duration, OccupationPattern pattern) {
         _start = start;
         _duration = duration;
-        _interval = _duration/occupancyCounts.Count;
-        _pattern = new OccupationPattern(occupancyCounts);
+        _interval = _duration / pattern.Count;
+        _pattern = pattern;
     }
 
     public int Max() => _state.Max(this);
 
     public double Average() => _state.Average(this);
+
+    public static Utilization operator +(Utilization left, Utilization right) => new(
+        new[] { left._start, right._start }.Min(),
+        left._duration,
+        left._pattern + right._pattern);
 
     private interface State {
         int Max(Utilization u);
@@ -28,13 +34,39 @@ public class Utilization {
 
     private class Initial : State {
         internal static readonly Initial Instance = new Initial();
-        private Initial() {}
+        private Initial() { }
         public int Max(Utilization u) => u._pattern.Max();
         public double Average(Utilization u) => u._pattern.Average();
     }
-    
-    private class OccupationPattern(List<int> occupancyCounts) {
-        internal int Max() => occupancyCounts.Max();
-        internal double Average() => occupancyCounts.Average();
+
+    private class OccupationPattern {
+        private readonly List<int> _occupancyCounts;
+
+        internal OccupationPattern(List<int> occupancyCounts) => 
+            _occupancyCounts = occupancyCounts;
+
+        internal int Count => _occupancyCounts.Count;
+
+        internal int Max() => _occupancyCounts.Max();
+
+        internal double Average() => _occupancyCounts.Average();
+
+        public static OccupationPattern operator +(
+            OccupationPattern left,
+            OccupationPattern right
+        ) => new(left._occupancyCounts
+            .Zip(right._occupancyCounts)
+            .Select(tuple => tuple.First + tuple.Second)
+            .ToList());
+
+        private IEnumerable<List<int>> Split(IList<int> source, int n) {
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(n);
+            var size = (int)Math.Ceiling(source.Count / (double)n);
+            // if (source.Count % n != 0)
+            //     throw new ArgumentException("List length must be divisible by n.");
+
+            for (var i = 0; i < source.Count; i += size)
+                yield return source.Skip(i).Take(size).ToList();
+        }
     }
 }
